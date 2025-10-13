a7fb50205d3ea6501fde32a5a574ce38d485d363
TheOvergrownSnail <tamir.finesilver777@gmail.com>
2025-10-13 15:59:37 -0600

    Testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoBildaKitBotTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoBildaKitBotTeleop.java
@@ -57,7 +57,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Since the dynamics of a launcher wheel system varies greatly from those of most other FTC mechanisms,
  * we will also need to adjust the "PIDF" coefficients with some that are a better fit for our application.
  */
-
+//Test
 @TeleOp(name = "GoBildaKitBotTeleop", group = "StarterBot")
 //@Disabled
 public class GoBildaKitBotTeleop extends OpMode {
