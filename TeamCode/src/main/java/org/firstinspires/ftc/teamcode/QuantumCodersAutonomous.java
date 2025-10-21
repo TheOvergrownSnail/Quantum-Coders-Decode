12311abc491dbd9e71790a9774758335763050bc
TheOvergrownSnail <tamir.finesilver777@gmail.com>
2025-10-21 17:12:44 -0600

    I made an autonomous code that sucks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/QuantumCodersAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/QuantumCodersAutonomous.java
@@ -1,4 +1,34 @@
+
 package org.firstinspires.ftc.teamcode;
 
-public class QuantumCodersAutonomous {
+import android.view.animation.GridLayoutAnimationController;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+public abstract class QuantumCodersAutonomous extends OpMode
+{
+    DcMotor motorLeft;
+    DcMotor motorRight;
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    ColorSensor color1;
+    DistanceSensor distance1;
+
+
+
+    public void runOpMode() throws InterruptedException {
+
+        color1 = hardwareMap.get(ColorSensor.class, "color1");
+        distance1 = hardwareMap.get(DistanceSensor.class, "distance1");
+        BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
+        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+        motorRight.setDirection(DcMotor.Direction.FORWARD);
+        Thread.sleep(500);
+    }
+
 }
